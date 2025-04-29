f991aeb90cece330f9b10557deae1c2b5e06d68d
Lahiru Bandara <73198791+Lahiru-Chathumina@users.noreply.github.com>
2025-04-29 11:10:40 +0530

    Update config.java
    @Configuration
    
    create

--- src/main/java/edu/lahiru_chathumina_bandara/ecom/config/config.java
+++ src/main/java/edu/lahiru_chathumina_bandara/ecom/config/config.java
@@ -2,7 +2,9 @@ package edu.lahiru_chathumina_bandara.ecom.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class config {
     @Bean
     public ModelMapper getMapper(){
