6b206d6998a3dfd6cf16ab8372639ed51d354d06
Lahiru-Bandara <chathuminabandaralahiru@gmail.com>
2025-04-29 10:03:07 +0530

    Initial project setup with pom.xml and src

---
+++ src/main/java/edu/lahiru_chathumina_bandara/ecom/Main.java
@@ -0,0 +1,13 @@
+package edu.lahiru_chathumina_bandara.ecom;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
